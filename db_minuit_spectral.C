4aefb20907f602a3ed017f8f2c1f70b34fd2d013
Mario Acero O <maaceroo@users.noreply.github.com>
2017-08-12 11:07:00 -0500

    Add files via upload
    - db_chi2_min.cpp find the minimum chi2 value using a file with three columns, where the first two are oscillation parameters and the third is the chi2 value.
    
    -db_margin.cpp produces the marginalization over one of the two oscillation parameters.
    
    - db_osc_spec_V2.C generates oscillated spectra for different values of the oscillation parameters and for each of the six Days Bay ADs.
    
    - db_minuit_spectral.C: This macro can be executed under ROOT typing "root[0] .x db_minuit_spectral.C". In the simplest case (only minimization) the script will print the message "Minimum: f(x[i]): chi^2(min)" where x[i] are the values of the parameters which minimize the chi^2-function and chi^2(min) is the minimum value of the chi^2-function. In the complete case, the results are printed in a  file, with the following information:        sin^2(2th_13)  delta(m31)^2  chi^2(min). In this case, chi^2(min) is the minimum chi^2 for  the pull terms.
    
    - db_minuit.C: Analysis of the Daya Bay data from F.P.

---
+++ db_minuit_spectral.C
@@ -0,0 +1,386 @@
+//----------------------------------------------------------//
+//-- db_minuit_spectral.C - By M.A. Acero O. - 2016-04-11 --//
+//----------------------------------------------------------//
+//-- Using 'NumericalMinimization.C' macro example to     --//
+//-- minimize a chi2 function. It uses a Minimizer class  --//
+//-- in ROOT.                                             --//
+//-- input : minimizer name + algorithm name              --//
+//-- randomSeed: = <0 : fixed value: 0 random with seed 0;--//
+//-- >0 random with given seed.                           --//
+//----------------------------------------------------------//
+//-- Analysis of the Daya Bay data from F.P. An et al.,   --//
+//-- PRL112 061801 (2014)                                 --//
+//----------------------------------------------------------//
+//-- This macro can be executed under ROOT typing "root[0] .x db_minuit_spectral.C" --//
+//-- In the simplest case (only minimization) the script will print the message     --//
+//-- "Minimum: f(x[i]): chi^2(min)" where x[i] are the values of the parameters     --//
+//-- which minimize the chi^2-function and chi^2(min) is the minimum value of the   --//
+//-- chi^2-function. In the complete case, the results are printed in a  file, with --//
+//-- the following information:        sin^2(2th_13)  delta(m31)^2  chi^2(min)      --//
+//-- In this case, chi^2(min) is the minimum chi^2 for  the pull terms.             --//
+//---*****************************************************---//
+//-- FOR THE SPECTRAL ANALYSIS ONLY                        --//
+//---*****************************************************---//
+//---*****************************************************---//
+//---------- CONSTANTS --------------------------------------//
+//---*****************************************************---//
+// histogram binning for the simulated data
+#define  NB 26
+#define  lo 0.7
+#define  hi 12.0
+//Number of Antineutrino Detectors
+#define nAD 6
+//Number of Nuclear Reactors
+#define nNR 6
+//Fixed neutrino oscillations parameters
+#define dm2_21 7.59e-5 //eV^2,                //PRL 108 171803 (2012)
+#define s22th_12 0.861
+//For the sin^2(2th_13) loop
+#define N_s2t  10                           //number of points in the grid
+#define lo_s2t 0.01                         //sin^2(2th_13) min
+//#define lo_s2t 0.0                          //sin^2(2th_13) min
+#define hi_s2t 0.3                          //sin^2(2th_13) max
+//For the delta(m31)^2 loop
+#define N_dm2  10                           //number of points in the grid
+#define lo_dm2 1.0e-4                       //delta(m31)^2 min
+#define hi_dm2 1.0e-2                       //delta(m31)^2 max
+//---*****************************************************---//
+
+//---*****************************************************---//
+//-- Global variables and quantities ------------------------//
+//---*****************************************************---//
+//EH1(AD1, AD2),EH2(AD3),EH3(AD4, AD5, AD6)
+//(IBD candidates)/(DAQ live time -days-) from PRL 112 061801 (2014)
+double IBDrate_data[nAD][2] = { {530.31,1.67},{536.75,1.68},{489.93,1.61},{ 73.58,0.62},{ 73.21,0.62},{72.35,0.62} };
+//IBD rate (per day), total background and efficiencies (PRL 112 061801 (2014))
+double totalBgd[nAD][2]     = { {13.20,0.98},{13.01,0.98},{ 9.57,0.71},{ 3.52,0.14},{ 3.48,0.14},{3.43,0.14} };
+double emuem[nAD]           = {0.7957,0.7927,0.8282,0.9577,0.9568,0.9566};
+double daqTime[nAD]         = {191.001,191.001,189.645,189.779,189.779,189.779};
+//---*****************************************************---//
+// Information obtained by executing the script "db_osc_rate.C"
+//IBD rate per day w/o oscillations
+double noOsc_IBDrate_perday[nAD] = {663.15,673.95,591.86,78.75,78.46,77.58};
+//<sin^2(1.267 dm2_21 L/E)> for each AD
+double avgSinDelta21[nAD]        = {0.000225897,0.000220442,0.000244761,0.00194541,0.00193977,0.00195247};
+//<sin^2(1.267 dm2_31 L/E)> for each AD
+double avgSinDelta31[nAD]        = {0.162935,0.159482,0.183415,0.750218,0.75162,0.752462};
+//---*****************************************************---//
+//const int dim = N_s2t*N_dm2;
+double s2th_13;     //oscillation parameter to be fitted
+double dm2_31;      //oscillation parameter to be fitted
+double wrd_array[nAD][nNR];
+double epsilon;     //normalization factor
+double alpha[nNR];  //'uncorrelated reactor uncertainty' pull term
+double eps_d[nAD];  //'uncorrelated detection uncertainty' pull term
+double eta_d[nAD];  //'background' pull term
+double spc[nAD][NB];
+double NoscTot[nAD];
+double noNoscTot[nAD];
+int iAD;
+//---*****************************************************---//
+const int nEH = 3;
+TH1F *data_spect_histo[nEH];
+TH1F *BkGd_spect_histo[nEH];
+//---*****************************************************---//
+TH1F *nosc_spect_hist[nAD];
+TH1F *wosc_spect_hist[nAD];
+TH1F *bfit_spect_hist[nAD];
+
+//---*****************************************************---//
+//-- Chi-square function to be minimized --------------------//
+//-- It has 18 pull parameters, 1 oscillation parameter and--//
+//-- a normalization factor. The last two are to be fitted. -//
+//---*****************************************************---//
+double chi2(const double *xx)
+{
+    //Pull parameters considered in the chi² function
+    eps_d[0] = xx[0];
+    eps_d[1] = xx[1];
+    eps_d[2] = xx[2];
+    eps_d[3] = xx[3];
+    eps_d[4] = xx[4];
+    eps_d[5] = xx[5];
+    eta_d[0] = xx[6];
+    eta_d[1] = xx[7];
+    eta_d[2] = xx[8];
+    eta_d[3] = xx[9];
+    eta_d[4] = xx[10];
+    eta_d[5] = xx[11];
+    alpha[0] = xx[12];
+    alpha[1] = xx[13];
+    alpha[2] = xx[14];
+    alpha[3] = xx[15];
+    alpha[4] = xx[16];
+    alpha[5] = xx[17];
+    epsilon  = xx[18];
+  
+    //---*****************************************************---//
+    int iNR;
+    double SurvPavg     = 0.0;
+    double sqr_chi      = 0.0;
+    double sqrerror     = 0.0;
+    double Md           = 0.0;
+    double Td           = 0.0;
+    double Bd           = 0.0;
+    double sB           = 0.0;
+    double seps_d       = 1.0*0.002;
+    double salph_r      = 1.0*0.008;
+    
+    for (iAD = 0 ; iAD < nAD ; iAD++){
+        SurvPavg = NoscTot[iAD]/noNoscTot[iAD];
+        for (int iBIN = 0 ; iBIN < NB ; iBIN++)
+        {
+            //-- Survival Probability equation. Terms depending on dM²_21 and dM²_32(~dM²_31) are averaged
+            //SurvP = 1.0 - 0.25*pow((1.0 + sqrt(1.0 - s2th_13)),2)*(s22th_12)*(avgSinDelta21[iAD]) - s2th_13*(avgSinDelta31[iAD]);
+        
+            //-- Predicted IBD from neutrino oscillations of the dth Antineutrino Detector
+            //Td = (SurvP * noOsc_IBDrate_perday[iAD])*emuem[iAD]*daqTime[iAD];
+            Td = spc[iAD][iBIN]*(SurvPavg[iAD]*noOsc_IBDrate_perday[iAD]/NoscTot[iAD])*emuem[iAD]*daqTime[iAD];
+            //-- Measured IDB events of the dth Antineutrino Detector (background is substracted)
+            //Md = (IBDrate_data[iAD][0] - totalBgd[iAD][0]*emuem[iAD])*daqTime[iAD];
+            int idx = -1;
+            if (iAD < 2) {
+                idx = 0;
+            }
+            else if (iAD == 2) {
+                idx = 1;
+            } else {
+                idx = 2;
+            }
+            double data_sigplusbg = (data_spect_histo[idx]->GetBinContent(iBIN+1))*IBDrate_data[iAD][0];
+            double simu_bg = (BkGd_spect_histo[idx]->GetBinContent(iBIN+1))*totalBgd[iAD][0]*emuem[iAD];
+            Md = (data_sigplusbg - simu_bg)*daqTime[iAD];
+            //-- Background of the dth Antineutrino Detector
+            //Bd = totalBgd[iAD][0]*emuem[iAD]*daqTime[iAD];
+            Bd = simu_bg;
+	
+            sqrerror = Md + Bd;
+	
+            //-- Fraction of IBD contribution of the rth reactor to the dth AD
+            //-- determined by baselines and reactor fluxes
+            double wrd = 0.0;
+            for (iNR = 0 ; iNR < nNR ; iNR++)
+                wrd += wrd_array[iAD][iNR]*alpha[iNR];
+        
+            //-- Testing a fake normalization factor
+            double test = 0.0;
+            sqr_chi += pow( (Md - Td*(1.0 + epsilon - test + eps_d[iAD] + wrd) + eta_d[iAD]) ,2 )/sqrerror;
+
+        }
+    }
+  
+    for (iAD = 0 ; iAD < nAD ; iAD++)
+        {
+            //-- Background error of the dth Antineutrino Detector
+            sB = totalBgd[iAD][1]*emuem[iAD]*daqTime[iAD];
+            sqr_chi += pow(eps_d[iAD]/seps_d,2) + pow(eta_d[iAD]/sB,2);
+        }
+    
+    for (iNR = 0 ; iNR < nNR ; iNR++)
+        sqr_chi += pow(alpha[iNR]/salph_r,2);
+
+
+    return sqr_chi;
+}
+//---*****************************************************---//
+
+int db_minuit_spectral(const char * minName = "Minuit",
+                       const char *algoName = "" ,
+                       int randomSeed = +10)
+    //int randomSeed = -1)
+{
+    cout << "Let's begin..." << endl;
+  
+    TFile *wrd_File = new TFile("../Docs_Visita-AAquilar-nov2015/daya-bay/baselines/daya-bay-ldist_6x6.root","READ");
+    TH1F *wrd_histo = (TH1F*)(wrd_File->Get("histo_ldist_6x6"));
+  
+    //-------------------
+    // Energy Histograms
+    //-------------------
+    TFile *fenergy = new TFile("PRL112_data.root","read");
+    //Three sets of histograms one for each Experimental Hall
+    for (int i = 0 ; i < nEH ; i++)
+        {
+            //Data
+            data_spect_histo[i] = (TH1F*) fenergy->Get(Form("data_spect_histo_%d",i));
+            double dfactor = 1.0/data_spect_histo[i]->Integral();
+            data_spect_histo[i]->Scale(dfactor);
+            //Background
+            BkGd_spect_histo[i] = (TH1F*) fenergy->Get(Form("BkGd_spect_histo_%d",i));
+            double bfactor = 1.0/BkGd_spect_histo[i]->Integral();
+            BkGd_spect_histo[i]->Scale(bfactor);
+        }
+    
+    // histogram binning
+    double xbins[27];
+    xbins[0] = lo;
+    double delta_bins2 = (7.3 - 1.3)/24.;// = 0.25 MeV/bin
+    for (int i=0;i<(NB-1);i++)
+    {
+        xbins[i+1] = 1.3 + delta_bins2*i;
+    }
+    xbins[26] = hi;
+    for(int iAD = 0 ; iAD < nAD ; iAD++)
+    {
+        nosc_spect_hist[iAD] = new TH1F(Form("nosc_spect_hist_%d",iAD),"",NB,xbins);
+        wosc_spect_hist[iAD] = new TH1F(Form("wosc_spect_hist_%d",iAD),"",NB,xbins);
+        bfit_spect_hist[iAD] = new TH1F(Form("bfit_spect_hist_%d",iAD),"",NB,xbins);
+    }
+  
+  
+  //---*****************************************************---//
+  /*    //Random fluctuation to the data in order to try to get larger values of chi^2 (By A.Aguilar-Arevalo 2016-02-12)
+	TF1 *gg = new TF1("gg","exp(-0.5*(x/1.5)^2)",-10,10);
+	for (int i=0;i<nAD;i++)
+	{
+        noOsc_IBDrate_perday[i] += gg->GetRandom();
+        cout << noOsc_IBDrate_perday[i]<<"  ";
+	}
+  */
+  //---*****************************************************---//
+  
+    for (int blid = 0 ; blid < nAD*nNR ; blid++)
+        {
+            int id = (blid/nNR);
+            int ir = (blid - id*nNR);
+      
+            wrd_array[id][ir] = wrd_histo->GetBinContent(blid+1);
+            //cout << blid << "  " << wrd_array[id][ir] << endl;
+        }
+    //break;
+    cout << "wrd array -> Done..." << endl;
+  
+    cout << "Minimization settings..." << endl;
+    ROOT::Math::Minimizer* min = ROOT::Math::Factory::CreateMinimizer(minName, algoName);
+  
+    //-- Set tolerance , etc...
+    min->SetMaxFunctionCalls(1000000); // for Minuit/Minuit2
+    min->SetMaxIterations(10000);      // for GSL
+    min->SetTolerance(0.001);
+    min->SetPrintLevel(-1);
+  
+    //-- File to print oscillation parameters and chi2 values
+    ofstream chi2Surface_file;
+    string s2t_eps = "chi2_s2t-eps_surface_SPEC.txt";  //(sin^2(2th13), epsilon, chi^2_min)
+    //string s2t_eps = "chi2_s2t_curve.txt"; //(sin^2(2th13), chi^2_min)
+    chi2Surface_file.open((s2t_eps).c_str());
+  
+    //-- File to print minimized pull parameters
+  
+    ofstream minimPullT_file;
+    string PullT = "chi2_pullT_surface.txt";
+    minimPullT_file.open((PullT).c_str());
+  
+    ifstream file("db_SurvParams_V2.txt");
+    cout << "Reading file - Loop in progress..." << endl;
+    int iad = 0;
+    int first6 = 1;
+    
+    
+    while (file >> iAD >> s2th_13 >> dm2_31 >> spc[iad][0] >> spc[iad][1] >> spc[iad][2] >> spc[iad][3] >> spc[iad][4] >> spc[iad][5] >> spc[iad][6] >> spc[iad][7] >> spc[iad][8] >> spc[iad][9] >> spc[iad][10] >> spc[iad][11] >> spc[iad][12] >> spc[iad][13] >> spc[iad][14] >> spc[iad][15] >> spc[iad][16] >> spc[iad][17] >> spc[iad][18] >> spc[iad][19] >> spc[iad][20] >> spc[iad][21] >> spc[iad][22] >> spc[iad][23] >> spc[iad][24] >> spc[iad][25] >> NoscTot[iad])
+        {//file loop
+            if(first6 <= 6) //Must go up to 6 to take the 6 ADs (I think) ---- 2017-08-07
+            {
+                for(int ibin = 1 ; ibin <= 26 ; ibin++)
+                {
+                    double bincont = spc[iad][ibin]*(noOsc_IBDrate_perday[iAD-1]/NoscTot[iad])*emuem[iAD-1]*daqTime[iAD-1];
+                    nosc_spect_hist[iAD-1]->SetBinContent(ibin,bincont);
+                }
+                cout << "iad = " << iad << "   iAD = " << iAD  << "   first6 = " << first6 << endl; // ---- 2017-08-07
+                //nosc_spect_hist[iAD-1]->Print("all"); // ---- 2017-08-07
+                cout << "Number of events: " << nosc_spect_hist[iAD-1]->Integral() << endl; // ---- 2017-08-07
+                //cout << "NoscTot         : " << NoscTot[iad] << endl; // ---- 2017-08-07
+                noNoscTot[iad] = NoscTot[iad];
+                first6++;
+                //continue; // - Commenting out ---- 2017-08-07
+            }//if first6 loop END
+            
+            //cout << iad+1 << ": " << iAD << " " << s2th_13 << " " << dm2_31 << " " ;
+            //for (int ib = 0; ib < NB; ib++) {
+            //cout << spc[iad][ib] << " ";
+            //}
+            //cout << NoscTot[iad] << " " << sp << endl;
+
+            iad++;
+            
+            //At this point, we have read the six AD spectra (six lines) for one point (s2th_13,dm2_31) in the grid
+            if (iad == 6)
+            {//if iad BEGIN
+                //cout << endl;
+                iad = 0;
+                
+                //This is executed for every 6 lines of the file
+                const int N_params = 19; //-- Number of parameter of the chi² function --//
+                ROOT::Math::Functor f(&chi2,N_params); //-- Setting the function to be minimized by using Minuit --//
+                //-- Steps
+                double stp = 1.0e-3;
+                double step[N_params] = {stp,stp,stp,stp,stp,stp,
+                                        stp,stp,stp,stp,stp,stp,
+                                        stp,stp,stp,stp,stp,stp,stp};
+                //-- Initial parameter values
+                double start[N_params] = {0.0,0.0,0.0,0.0,0.0,0.0,
+                                        0.0,0.0,0.0,0.0,0.0,0.0,
+                                        0.0,0.0,0.0,0.0,0.0,0.0,0.0};
+                
+                //-- Calling Minuit function setting
+                min->SetFunction(f);
+                
+                //-- Setting variables
+                double lim = 1.0e-3;
+                min->SetLimitedVariable(0,  "e_1", start[0],  step[0],  -lim, lim);
+                min->SetLimitedVariable(1,  "e_2", start[1],  step[1],  -lim, lim);
+                min->SetLimitedVariable(2,  "e_3", start[2],  step[2],  -lim, lim);
+                min->SetLimitedVariable(3,  "e_4", start[3],  step[3],  -lim, lim);
+                min->SetLimitedVariable(4,  "e_5", start[4],  step[4],  -lim, lim);
+                min->SetLimitedVariable(5,  "e_6", start[5],  step[5],  -lim, lim);
+                min->SetLimitedVariable(6,  "n_1", start[6],  step[6],  -lim, lim);
+                min->SetLimitedVariable(7,  "n_2", start[7],  step[7],  -lim, lim);
+                min->SetLimitedVariable(8,  "n_3", start[8],  step[8],  -lim, lim);
+                min->SetLimitedVariable(9,  "n_4", start[9],  step[9],  -lim, lim);
+                min->SetLimitedVariable(10, "n_5", start[10], step[10], -lim, lim);
+                min->SetLimitedVariable(11, "n_6", start[11], step[11], -lim, lim);
+                min->SetLimitedVariable(12, "a_1", start[12], step[12], -lim, lim);
+                min->SetLimitedVariable(13, "a_2", start[13], step[13], -lim, lim);
+                min->SetLimitedVariable(14, "a_3", start[14], step[14], -lim, lim);
+                min->SetLimitedVariable(15, "a_4", start[15], step[15], -lim, lim);
+                min->SetLimitedVariable(16, "a_5", start[16], step[16], -lim, lim);
+                min->SetLimitedVariable(17, "a_6", start[17], step[17], -lim, lim);
+                //min->SetLimitedVariable(18, "eps", start[18], step[18], -lim, lim);
+                min->SetFixedVariable(18, "eps", start[18]);
+                min->SetErrorDef(2.3);
+                
+                //-- Calling Minuit minimization
+                min->Minimize();
+          
+                const double *xs = min->X();
+                
+                chi2Surface_file << s2th_13 << "\t" << dm2_31 << "\t" << min->MinValue() << endl;
+                
+                minimPullT_file  << s2th_13 << "\t" << dm2_31 << "\t" << xs[0] << "\t" << xs[1] << "\t" << xs[2] << "\t" << xs[3] << "\t" << xs[4] << "\t" << xs[5] << "\t" << xs[6] << "\t" << xs[7] << "\t" << xs[8] << "\t" << xs[9] << "\t" << xs[10] << "\t" << xs[11] << "\t" << xs[12] << "\t" << xs[13] << "\t" << xs[14] << "\t" << xs[15] << "\t" << xs[16] << "\t" << xs[17] << "\t" << xs[18] << "\t" << min->MinValue() << endl;
+                //}
+                if (dm2_31 == hi_dm2)
+                {
+                    chi2Surface_file << endl;
+                    cout << s2th_13 << "\t" << dm2_31 << "\t" << min->MinValue() << endl;
+                }
+                //minimPullT_file  << endl;
+                //if (is2t%10 == 0)
+                //std::cout << "Succesful run for sin^2(th13) = " << s2th_13 << "!! \t" << min->MinValue() << endl;
+                //}
+            }//if iad END
+            
+        }//file loop END
+    std::cout << "Succesful run!!" << endl;
+
+    //Drawing no-oscillated spectra
+    TCanvas *canv0 = new TCanvas("canv0","",3*700,2*500);
+    canv0->Divide(3,2);
+    for(int iAD = 0 ; iAD < 6 ; iAD++)
+    {
+        canv0->cd(iAD+1);
+        nosc_spect_hist[iAD]->Draw("");
+    }
+    canv0->Print("canv0.pdf");
+
+    return 0;
+}
---
+++ db_ntuple.C
@@ -0,0 +1,93 @@
+//---------------------------------------------------------------------//
+//--  db_ntuple.C - By M.A. Acero O. & A.A. Aguilar-A. - 2016-01-02  --//
+//---------------------------------------------------------------------//
+// This macro can be executed under ROOT typing                        //
+// "root[0] .x db_ntuple.C"                                            //
+// For the spectral analysis, using information from:                  //
+// - F.P. An et al., PRL 112 061801 (2014)                             //
+// - F.P. An et al., Chin.Phys.C37 011001 (2013)                       //
+//---------------------------------------------------------------------//
+// Simulated events, following the Daya Bay energy spectra from the    //
+// paper PRL112.                                                       //
+//---------------------------------------------------------------------//
+
+#include<constants.h>
+
+void db_ntuple()
+{ // begin
+
+    //-------------------
+    // Energy Histograms
+    //-------------------
+    TFile *fenergy = new TFile("PRL112_data.root","read");
+    //Three sets of histograms one for each Experimental Hall
+    const int nEH = 3;
+    TH1F *data_spect_histo[nEH];
+    for (int i = 0 ; i < nEH ; i++)
+    {
+        data_spect_histo[i] = (TH1F*) fenergy->Get(Form("data_spect_histo_%d",i));
+    }
+
+    //-------------------
+    // Distance Histogram
+    //-------------------
+    TFile *fpathl = new TFile("../Docs_Visita-AAquilar-nov2015/daya-bay/baselines/daya-bay-ldist.root","read");
+    //Only 6 detectors (PRL112 061801, 2014)
+    TH1F *histo_ldist_6Det = (TH1F*) fpathl->Get("histo_ldist_6Det");
+
+    const int nDet = 8;
+    const int nRea = 6;
+    //Baseline Distances (cm)
+    char  *detNames[nDet] = {"EH1-AD1", "EH1-AD2", "EH2-AD1", "EH2-AD2",
+                             "EH3-AD1", "EH3-AD2", "EH3-AD3", "EH3-AD4"};
+    //The 48 baselines in Daya-Bay ()
+    double baselines[nDet][nRea] =
+    {
+        {362.380, 371.763,  903.466, 817.158,1353.618,1265.315},
+        {357.940, 368.414,  903.347, 816.896,1354.229,1265.886},
+		{1332.479,1358.148, 467.574, 489.577, 557.579, 499.207},
+		{1337.429,1362.876, 472.971, 495.346, 558.707, 501.071},
+		{1919.632,1894.337,1533.180,1533.628,1551.384,1524.940},
+		{1917.519,1891.977,1534.919,1535.032,1554.767,1528.079},
+		{1925.255,1899.861,1538.930,1539.468,1556.344,1530.078},
+		{1923.149,1897.507,1540.667,1540.872,1559.721,1533.179}
+    };
+
+    //make ntuple
+    TFile *fout = new TFile("db-ntuple.root","RECREATE");
+    TTree *T = new TTree("T","Monte Carlo neutrino events");
+
+    float Ep, En, Ln;
+    int   blid,ir,id,ad;
+    T->Branch("Ep"  ,&Ep  ,"Ep/F");	  //prompt reconstructed energy
+    T->Branch("En"  ,&En  ,"En/F");	  //neutrino energy
+    T->Branch("Ln"  ,&Ln  ,"Ln/F");	  //neutrino baseline
+    T->Branch("blid",&blid,"blid/s"); //Baseline id (0-47)
+    
+    T->Branch("ir", &ir, "ir/s"); //reactor
+    T->Branch("id", &id, "id/s"); //detector
+
+    int Nevents = 10000000;
+        for (int i = 0 ; i < Nevents ; i++)
+        {
+            // generate a baseline (blid uniquely identifies the baseline)
+            blid = (int*) histo_ldist_6Det->GetRandom();
+            id =   (int*) (blid/nRea);
+            ir =   (int*) (blid - id*nRea);
+            Ln = baselines[id][ir];
+            
+            // generate a neutrino energy
+            //if (id is 0 or 1, espectro de EH1; id is 2, espectro de EH2; id is 4 to 6,espectro de EH3) esto para Ep
+            if (id < 2)       ad = 0;
+            else if (id == 2) ad = 1;
+            else if (id > 3 && id < 7) ad = 2;
+            
+            Ep = data_spect_histo[ad]->GetRandom();
+            En = Ep + avg_nRecoilE + avg_constE;
+        
+            T->Fill();
+        }
+    
+    fout->Write();
+
+} // end
