17277fbea5de2f77ae6d59a5a6d3c018c38896cb
Mario AAO <maaceroo@gmail.com>
2017-08-18 18:02:26 -0500

    Survival probability function for the rate-only analysis
    Signed-off-by: Mario AAO <maaceroo@gmail.com>

---
+++ rate_SurvProb.C
@@ -0,0 +1,15 @@
+double rate_SurvProb(double *x, double *par)
+{//begin
+
+    double s2t = x[0];
+    
+    double avgD21 = par[0];
+    double avgD31 = par[1];
+    
+    double s22th_12 = 0.861;
+    
+    double SProb = 1.0 - 0.25*((1.0 + sqrt(1.0 - s2t))**2)*s22th_12*avgD21 - s2t*avgD31;
+    
+    return SProb;
+    
+}//end
