5a3d4c283380955b3e506f66a0afee07331e37a6
Mario Acero <maaceroo@gmail.com>
2019-05-03 18:00:20 -0500

    Updating db_CorrMatrix_6AD8ADx35bins.C to create the full (6+8)AD Correlation Matrix
    Signed-off-by: Mario Acero <maaceroo@gmail.com>

--- DB_SPEC_8AD/CombCovaMat/db_CorrMatrix_6AD8ADx35bins.C
+++ DB_SPEC_8AD/CombCovaMat/db_CorrMatrix_6AD8ADx35bins.C
@@ -30,8 +30,8 @@ void db_CorrMatrix_6AD8ADx35bins()
 
     //--6AD 8x8 correlation matrix
     TFile *fmat6 = new TFile("db_CorrMatrix_6AD_35bins.root");
-    TH2F *corrMat_histo_6Det_8x8 = (TH2F*)fmat6->Get("corrMat_histo_8x8_6Det");
-    cout << "6AD Bins: " << corrMat_histo_6Det_8x8->GetXaxis()->GetNbins() << endl;
+    TH2F *corrMat_histo_6Det_1x1 = (TH2F*)fmat6->Get("corrMatRebHisto");
+    cout << "6AD Bins: " << corrMat_histo_6Det_1x1->GetXaxis()->GetNbins() << endl;
     //--8AD 1x1 correlation matrix
     TFile *fmat8 = new TFile("db_CorrMatrix_8AD_35bins.root");
     TH2F *corrMat_histo_8Det_1x1 = (TH2F*)fmat8->Get("corrMatRebHisto");
@@ -39,29 +39,149 @@ void db_CorrMatrix_6AD8ADx35bins()
     //matrix definition
     
     //---------------------------------------------------------------------------
-    //-- buildon the large (8*35)x(8*35) 8AD Correlation matrix
+    //---------------------------------------------------------------------------
+    // 8x8 correlation matrix (constructed by hand ... and by "eye")
+    
+    double rho_array[8][8]={{1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0},
+                            {0.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0},
+        {0.0,0.0,1.0,0.0,0.0,0.0,0.0,0.0},
+        {0.0,0.0,0.0,1.0,0.0,0.0,0.0,0.0},
+        {0.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0},
+        {0.0,0.0,0.0,0.0,0.0,1.0,0.0,0.0},
+        {0.0,0.0,0.0,0.0,0.0,0.0,1.0,0.0},
+        {0.0,0.0,0.0,0.0,0.0,0.0,0.0,1.0}};
+    
+    //Construct correlation matrix upper triangle
+    double corr_start = 0.999;
+    for (int j = 0 ; j < 8 ; j++){
+        for (int k = 0 ; k < 8 ; k++){
+            for (int i = 0 ; i < 8 ; i++) {
+                int ij = i+j;
+                int ik = i+k;
+                if ( (ij)<8 && (ik<8) ) {
+                    if (ij==ik)
+                        rho_array[ij][ik]=1.0;
+                    else if ( ik==(ij+1) )
+                        rho_array[ij][ik]=corr_start;
+                    else if ( ij<(8-1) )
+                        rho_array[ij][ik] = rho_array[ij][ik-1]*rho_array[ij+1][ik];
+                }//if
+            }//for i
+        }//for k
+    }//for j
+    
+    //Assign correlation matrixlower triangle
+    double corrMat_8x8Block[8][8];
+    for(int ii = 0 ; ii < 8 ; ii++){
+        for(int jj = ii+1 ; jj < 8 ; jj++){
+            rho_array[jj][ii] = rho_array[ii][jj];
+        }
+    }
+    
+    cout << "The AD-correlation Matrix is:" << endl;
+    for(int ii = 0 ; ii < 8 ; ii++){
+        for(int jj = 0 ; jj < 8 ; jj++){
+            corrMat_8x8Block[ii][jj] =  rho_array[ii][jj];
+            cout << setprecision(4) << corrMat_8x8Block[ii][jj] << "\t";
+        }
+        cout << endl;
+    }
+    //-- building the large (8*35)x(8*35) 6AD Correlation matrix
     //-- Number of detectors
-    int    NBx_8x8 = corrMat_histo_6Det_8x8->GetXaxis()->GetNbins();
+    int NBx = NB;
+    int NBy = NB;
+    int nDet = 6+2;
+    int    NBx_8x8 = NBx*nDet;
     double lox_8x8 = 0;
-    double hix_8x8 = NBx_8x8;
-    int    NBy_8x8 = corrMat_histo_6Det_8x8->GetYaxis()->GetNbins();
+    double hix_8x8 = NBx*nDet;
+    int    NBy_8x8 = NBy*nDet;
     double loy_8x8 = 0;
-    double hiy_8x8 = NBy_8x8;
-    TH2F *corrMat_histo_8Det_8x8 = new TH2F("corrMat_histo_8Det_8x8","",NBx_8x8,lox_8x8,hix_8x8,NBy_8x8,loy_8x8,hiy_8x8);
+    double hiy_8x8 = NBy*nDet;
+    TMatrixD corrMat_mat_8x8_6Det(NBx_8x8,NBy_8x8);
+    TH2F *corrMat_histo_8x8_6Det = new TH2F("corrMat_histo_8x8_6Det","",NBx_8x8,lox_8x8,hix_8x8,NBy_8x8,loy_8x8,hiy_8x8);
     cout << NBx_8x8 << "  " << NBy_8x8 << endl;
     for (int i = 0 ; i < NBx_8x8 ; i++) {
         for (int j = i ; j < NBy_8x8 ; j++) {
-            int ii = i%NBx_8x8;
-            int jj = j%NBy_8x8;
-            int iB = int(i/NBx_8x8);
-            int jB = int(j/NBy_8x8);
+            int ii = i%NBx;
+            int jj = j%NBy;
+            int iB = int(i/NBx);
+            int jB = int(j/NBy);
+            double block_corrFact_ij = corrMat_8x8Block[iB][jB];
             //cout << i << " " << j << "   -   " << ii << " " << jj << "   -   " << iB << " " << jB << endl;
-            double value = corrMat_histo_8Det_1x1->GetBinContent(ii+1,jj+1);
-            corrMat_histo_8Det_8x8->SetBinContent(i+1,j+1,value);
-            corrMat_histo_8Det_8x8->SetBinContent(j+1,i+1,value);
+            double value = corrMat_histo_6Det_1x1->GetBinContent(ii+1,jj+1)*block_corrFact_ij;
+            if (iB == 3 || iB == 7)
+                value = 0.0;
+            if (jB == 3 || jB == 7)
+                value = 0.0;
+            corrMat_histo_8x8_6Det->SetBinContent(i+1,j+1,value);
+            corrMat_histo_8x8_6Det->SetBinContent(j+1,i+1,value);
+            corrMat_mat_8x8_6Det(i,j) = value;
+            corrMat_mat_8x8_6Det(j,i) = value;
         }
     }
-    break;
+    //---------------------------------------------------------------------------
+    //-- building the large (8*35)x(8*35) 8AD Correlation matrix
+    TMatrixD corrMat_mat_8x8_8Det(NBx_8x8,NBy_8x8);
+    TH2F *corrMat_histo_8x8_8Det = new TH2F("corrMat_histo_8x8_8Det","",NBx_8x8,lox_8x8,hix_8x8,NBy_8x8,loy_8x8,hiy_8x8);
+    cout << NBx_8x8 << "  " << NBy_8x8 << endl;
+    for (int i = 0 ; i < NBx_8x8 ; i++) {
+        for (int j = i ; j < NBy_8x8 ; j++) {
+            int ii = i%NBx;
+            int jj = j%NBy;
+            int iB = int(i/NBx);
+            int jB = int(j/NBy);
+            double block_corrFact_ij = corrMat_8x8Block[iB][jB];
+            //cout << i << " " << j << "   -   " << ii << " " << jj << "   -   " << iB << " " << jB << endl;
+            double value = corrMat_histo_8Det_1x1->GetBinContent(ii+1,jj+1)*block_corrFact_ij;
+            corrMat_histo_8x8_8Det->SetBinContent(i+1,j+1,value);
+            corrMat_histo_8x8_8Det->SetBinContent(j+1,i+1,value);
+            corrMat_mat_8x8_8Det(i,j) = value;
+            corrMat_mat_8x8_8Det(j,i) = value;
+        }
+    }
+    TMatrixD offDiagBlock_6DetX8Det(NBx_8x8,NBy_8x8);
+    offDiagBlock_6DetX8Det.Mult(corrMat_mat_8x8_6Det,corrMat_mat_8x8_8Det);
+    //---------------------------------------------------------------------------
+    double corrMat_2x2BigBlock[2][2] = {{1.0,0.99},{0.99,1.0}};
+    double value;
+    TH2F *corrMat_histo_6Det8Det = new TH2F("corrMat_histo_6Det8Det","",2*NBx_8x8,0,2*NBx_8x8,2*NBy_8x8,0,2*NBy_8x8);
+    //-- there is something worng here, with the matrix filling - 2019.05.03
+    for (int i = 0 ; i < 2*NBx_8x8 ; i++) {
+        for (int j = i ; j < 2*NBy_8x8 ; j++) {
+            if ( (i < NBx_8x8) && (j < NBy_8x8) ) {
+                value = corrMat_mat_8x8_6Det(i,j);
+                corrMat_histo_6Det8Det->SetBinContent(i+1,j+1,value);
+                corrMat_histo_6Det8Det->SetBinContent(j+1,i+1,value);
+            }
+            if ( (i >= NBx_8x8) && (j >= NBy_8x8) ) {
+                value = corrMat_mat_8x8_8Det(i-NBx_8x8,j-NBy_8x8);
+                corrMat_histo_6Det8Det->SetBinContent(i+1,j+1,value);
+                corrMat_histo_6Det8Det->SetBinContent(j+1,i+1,value);
+            }
+            if ( (i < NBx_8x8) && (j >= NBy_8x8) ) {
+                value = offDiagBlock_6DetX8Det(i,j-NBy_8x8);
+                corrMat_histo_6Det8Det->SetBinContent(i+1,j+1,value);
+                corrMat_histo_6Det8Det->SetBinContent(j+1,i+1,value);
+            }
+        }
+    }
+    
+    //---------------------------------------------------------
+    set_plot_style();
+    
+    TCanvas *canv0 = new TCanvas("canv0","",2*500,500);
+    canv0->Divide(2,1);
+    canv0->cd(1);
+    corrMat_histo_8x8_6Det->Draw("COLZ");
+    canv0->cd(2);
+    corrMat_histo_8x8_8Det->Draw("COLZ");
+    //---------------------------------------------------------
+    TCanvas *canv1 = new TCanvas("canv1","",500,500);
+    canv1->cd(1);
+    corrMat_histo_6Det8Det->Draw("COLZ");
+    
+
+    /*
     //---------------------------------------------------------------------------
 
     //--- Graph with errors from https://arxiv.org/pdf/1508.04233.pdf  Fig. 2
@@ -261,6 +381,6 @@ void db_CorrMatrix_6AD8ADx35bins()
     TLine* line1 = new TLine(gPad->GetUxmin(),1,gPad->GetUxmax(),1);
     line1->Draw();
     gPad->SetTicks(1,1);
-
+*/
 }// end
 
