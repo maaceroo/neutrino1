8e0cdedc3b7a1a9c96b7083dc39ce59421e10342
Mario Acero <maaceroo@gmail.com>
2021-11-13 17:07:44 -0600

    Modfied Energy Scale macro to create the necessary derivative function for the energy scale pull term (by A. Aguilar-Arevalo) Signed-off-by: Mario Acero <maaceroo@gmail.com>

--- MINOS/MINOS_EnergyS_ntuple.C
+++ MINOS/MINOS_EnergyS_ntuple.C
@@ -6,7 +6,7 @@
 //-- corresponding derivative to be used in the minumization process.              --//
 //-----------------------------------------------------------------------------------//
 //#include "EnergyS_constants.h" //CAUTION - To check
-//#include "constants.h"
+#include "constants.h"
 #include <math.h>
 #include <iostream>
 #include <string>
@@ -19,9 +19,9 @@
 //---*****************************************************---//
 double e;       //Escale Pull term
 // histogram binning for the data (PRL 110, 251801 (2013))
-const int NB_numu110    = 23;
-double    lo110         =  0.5;
-double    hi110         = 14.0;
+//const int NB_numu110    = 23;
+//double    lo110         =  0.5;
+//double    hi110         = 14.0;
 double xbins_numu110[NB_numu110+1];
 //Number of energy scale factors to test
 const int de = 6;
@@ -42,8 +42,9 @@ int MINOS_EnergyS_ntuple()
     cout << "Let's begin..." << endl;
 
     //-- File with the predicted spectra (for different oscillation paramenters)
+    TString filePath = dirName;
     cout << "Reading file - Loop in progress..." << endl;
-    TFile *fntuple = new TFile("./minos_ntuple.root","READ");
+    TFile *fntuple = new TFile(filePath + "/data/minos_ntuple.root","READ");
     TTree *Tnumu = (TTree*)fntuple->Get("Tnumu");
     TCut cutES;
 
@@ -94,14 +95,16 @@ int MINOS_EnergyS_ntuple()
     }//End for(ee)
 
     //-- Computing the difference produced by the energy scale
-    //- (Original - Corrected) spectra
+    //- (Scaled - Original) spectra
     for (int ee = 0 ; ee < de ; ee++) {
         e = lowe + ee*deltae;
         nosc_spect_hist_diff[ee] = new TH1F(*nosc_spect_hist);
+        nosc_spect_hist_diff[ee] ->SetName(Form("nosc_spec_hist_diff_%d",ee));
         nosc_spect_hist_deri[ee] = new TH1F(*nosc_spect_hist);
-        nosc_spect_hist_diff[ee]->GetYaxis()->SetTitle("Events(Original - Corrected)");
+        nosc_spect_hist_deri[ee] ->SetName(Form("nosc_spec_hist_deri_%d",ee));
+        nosc_spect_hist_diff[ee]->GetYaxis()->SetTitle("Events(Scaled - Original)");
         for (int i = 1 ; i <= NB_numu110 ; i++) {
-            double binCont = nosc_spect_hist->GetBinContent(i) - nosc_spect_hist_e[ee]->GetBinContent(i);
+            double binCont = nosc_spect_hist_e[ee]->GetBinContent(i) - nosc_spect_hist->GetBinContent(i);
             nosc_spect_hist_diff[ee]->SetBinContent(i,binCont);
             nosc_spect_hist_deri[ee]->SetBinContent(i,binCont/(e));
         }
@@ -110,20 +113,35 @@ int MINOS_EnergyS_ntuple()
     }
 
     TF1 *fFit4;
-    TF1 *fFit7;
     fFit4 = new TF1("fFit4","[0]+[1]*x+[2]*(x**2)+[3]*(x**3)+[4]*(x**4)",xbins_numu110[0],xbins_numu110[NB_numu110]);
     fFit4->SetParameters(0.0,0.0,0.0,0.0,0.0);
     nosc_spect_hist_deri[1]->Fit("fFit4","r");
     fFit4->SetLineStyle(2);
 
+    TF1 *fFit7;
     fFit7 = new TF1("fFit7","[0]+[1]*x+[2]*(x**2)+[3]*(x**3)+[4]*(x**4)+[5]*(x**5)+[6]*(x**6)+[7]*(x**7)",xbins_numu110[0],xbins_numu110[NB_numu110]);
     fFit7->SetParameters(0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0);
     nosc_spect_hist_deri[1]->Fit("fFit7","r");
+
+    TF1 *fFitGD;
+    fFitGD = new TF1("fFitGD","[0]+[1]*exp(-0.5*pow((x-[2])/[3],2))+[4]*exp(-0.5*pow((x-[5])/[6],2))",xbins_numu110[0],xbins_numu110[NB_numu110]);
+    fFitGD->SetParameters(0.0,-2e6,3.0, 2.0,1e6,5.0,2.0);
+    fFitGD->SetLineColor(kBlack);
+    nosc_spect_hist_deri[4]->Fit("fFitGD","r");
+
     
-    TFile *fEscale = new TFile("./minos_EScaleDerivative.root","recreate");
+    TFile *fEscale = new TFile(filePath + "/data/minos_EScaleDerivative.root","recreate");
     fFit4->Write();
     fFit7->Write();
-    
+    fFitGD->Write();
+
+    nosc_spect_hist->Write();
+    for (int i = 0; i<de; i++){
+      nosc_spect_hist_e[i]->Write();
+      nosc_spect_hist_diff[i]->Write();
+      nosc_spect_hist_deri[i]->Write();
+    }
+
     //-- Plotting section -----------------------//
     //-- Defining histogram frames --//
     double max_numu = 0.0, min_numu = 0.0;
@@ -188,7 +206,10 @@ int MINOS_EnergyS_ntuple()
         nosc_spect_hist_deri[ee]->Draw("same hist");
     fFit4->Draw("same");
     fFit7->Draw("same");
+    fFitGD->Draw("same");
 
+
+    
 
     std::cout << "Succesful run!!" << endl;
 
