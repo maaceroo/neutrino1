66572b0322f8b59c53f3412926d6e3ec2165be64
Alexis Aguilar-Arevalo <alexis@nucleares.unam.mx>
2023-09-15 16:07:15 -0600

    Changed to slurm job management system
    Signed-off-by: Alexis Aguilar-Arevalo <alexis@nucleares.unam.mx>

--- MINOS/MINOS_osc_ana_80x80.sh
+++ MINOS/MINOS_osc_ana_80x80.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #---------------------------------------------------------------
-export JOBID=`(echo $PBS_JOBID | cut -d. -f1)`
+export JOBID=`(echo $SLURM_JOB_ID | cut -d. -f1)`
 echo 'JOBID='${JOBID}
 #---------------------------------------------------------------
 #Define grid 
@@ -62,7 +62,7 @@ echo '=========================================='
 echo '2) Running MINOS_osc_2nu.C'
 echo '=========================================='
 echo
-time root -b -l -n -q MINOS_osc_2nu.C
+#time root -b -l -n -q MINOS_osc_2nu.C
 #
 #---------------------------------------------------------------
 # run minimization
@@ -70,106 +70,106 @@ echo '=========================================='
 echo '3) Running MINOS_minuit.C'
 echo '=========================================='
 echo
-root -b -l -n -q MINOS_minuit.C
+#root -b -l -n -q MINOS_minuit.C
 
 echo
 
 #---------------------------------------------------------------
 #Remove first line from file
-tail -n +2 ${JOBID}/data/numu_chi2_s2t-dm2_surface.txt > ${JOBID}/data/numu_chi2_s2t-dm2_surface-noFL.txt
-tail -n +2 ${JOBID}/data/numub_chi2_s2t-dm2_surface.txt > ${JOBID}/data/numub_chi2_s2t-dm2_surface-noFL.txt
-tail -n +2 ${JOBID}/data/numubWS_chi2_s2t-dm2_surface.txt > ${JOBID}/data/numubWS_chi2_s2t-dm2_surface-noFL.txt
+#tail -n +2 ${JOBID}/data/numu_chi2_s2t-dm2_surface.txt > ${JOBID}/data/numu_chi2_s2t-dm2_surface-noFL.txt
+#tail -n +2 ${JOBID}/data/numub_chi2_s2t-dm2_surface.txt > ${JOBID}/data/numub_chi2_s2t-dm2_surface-noFL.txt
+#tail -n +2 ${JOBID}/data/numubWS_chi2_s2t-dm2_surface.txt > ${JOBID}/data/numubWS_chi2_s2t-dm2_surface-noFL.txt
 
 #---------------------------------------------------------------
 #compile routines for minimization and marginalization
-echo '=========================================='
-echo 'compiling  minos_numu_chi2_min.cpp and minos_numu_margin.cpp'
-echo '=========================================='
-g++ -o minos_numu_chi2_min.exe minos_numu_chi2_min.cpp
-g++ -o minos_numu_margin.exe minos_numu_margin.cpp
-echo 'compiling  minos_numub_chi2_min.cpp and minos_numub_margin.cpp'
-echo '=========================================='
-g++ -o minos_numub_chi2_min.exe minos_numub_chi2_min.cpp
-g++ -o minos_numub_margin.exe minos_numub_margin.cpp
-echo 'compiling  minos_numubWS_chi2_min.cpp and minosWS_numub_margin.cpp'
-echo '=========================================='
-g++ -o minos_numubWS_chi2_min.exe minos_numubWS_chi2_min.cpp
-g++ -o minos_numubWS_margin.exe minos_numubWS_margin.cpp
+#echo '=========================================='
+#echo 'compiling  minos_numu_chi2_min.cpp and minos_numu_margin.cpp'
+#echo '=========================================='
+#g++ -o minos_numu_chi2_min.exe minos_numu_chi2_min.cpp
+#g++ -o minos_numu_margin.exe minos_numu_margin.cpp
+#echo 'compiling  minos_numub_chi2_min.cpp and minos_numub_margin.cpp'
+#echo '=========================================='
+#g++ -o minos_numub_chi2_min.exe minos_numub_chi2_min.cpp
+#g++ -o minos_numub_margin.exe minos_numub_margin.cpp
+#echo 'compiling  minos_numubWS_chi2_min.cpp and minosWS_numub_margin.cpp'
+#echo '=========================================='
+#g++ -o minos_numubWS_chi2_min.exe minos_numubWS_chi2_min.cpp
+#g++ -o minos_numubWS_margin.exe minos_numubWS_margin.cpp
 
 echo
 #---------------------------------------------------------------
-echo '=========================================='
-echo 'executing  minos_numu_chi2_numu_min.exe and minos_numu_margin.exe'
-./minos_numu_chi2_min.exe $NS2T $NDM2 ./${JOBID}
-./minos_numu_margin.exe $NS2T $NDM2 ./${JOBID}
-echo 'executing  minos_numub_chi2_min.exe and minos_numub_margin.exe'
-echo '=========================================='
-./minos_numub_chi2_min.exe $NS2T $NDM2 ./${JOBID}
-./minos_numub_margin.exe $NS2T $NDM2 ./${JOBID}
-echo 'executing  minos_numubWS_chi2_min.exe and minos_numubWS_margin.exe'
-echo '=========================================='
-./minos_numubWS_chi2_min.exe $NS2T $NDM2 ./${JOBID}
-./minos_numubWS_margin.exe $NS2T $NDM2 ./${JOBID}
-echo
+#echo '=========================================='
+#echo 'executing  minos_numu_chi2_numu_min.exe and minos_numu_margin.exe'
+#./minos_numu_chi2_min.exe $NS2T $NDM2 ./${JOBID}
+#./minos_numu_margin.exe $NS2T $NDM2 ./${JOBID}
+#echo 'executing  minos_numub_chi2_min.exe and minos_numub_margin.exe'
+#echo '=========================================='
+#./minos_numub_chi2_min.exe $NS2T $NDM2 ./${JOBID}
+#./minos_numub_margin.exe $NS2T $NDM2 ./${JOBID}
+#echo 'executing  minos_numubWS_chi2_min.exe and minos_numubWS_margin.exe'
+#echo '=========================================='
+#./minos_numubWS_chi2_min.exe $NS2T $NDM2 ./${JOBID}
+#./minos_numubWS_margin.exe $NS2T $NDM2 ./${JOBID}
+#echo
 
 #---------------------------------------------------------------
 #Extract BF_CHI2, BF_S2T, BF_DM2 from chi2_minumum_SPEC.txt
 
-read BF_NUMU_CHI2   BF_NUMU_S2T   BF_NUMU_DM2 <<< `cat ${JOBID}/data/numu_chi2_minumum.txt`
-read BF_NUMUB_CHI2  BF_NUMUB_S2T  BF_NUMUB_DM2 <<< `cat ${JOBID}/data/numub_chi2_minumum.txt`
-read BF_NUMUBWS_CHI2  BF_NUMUBWS_S2T  BF_NUMUBWS_DM2 <<< `cat ${JOBID}/data/numubWS_chi2_minumum.txt`
+#read BF_NUMU_CHI2   BF_NUMU_S2T   BF_NUMU_DM2 <<< `cat ${JOBID}/data/numu_chi2_minumum.txt`
+#read BF_NUMUB_CHI2  BF_NUMUB_S2T  BF_NUMUB_DM2 <<< `cat ${JOBID}/data/numub_chi2_minumum.txt`
+#read BF_NUMUBWS_CHI2  BF_NUMUBWS_S2T  BF_NUMUBWS_DM2 <<< `cat ${JOBID}/data/numubWS_chi2_minumum.txt`
 
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 # Edit gnuplot script
-echo '=========================================='
-echo 'Editting gnu plot scripts ...'
-echo
+#echo '=========================================='
+#echo 'Editting gnu plot scripts ...'
+#echo
 
-echo '--------------- numu --------------'
+#echo '--------------- numu --------------'
 
-sed -i'' -e "12s/.*/set output \"${JOBID}\/files_plots\/MINOS_plots_numu.pdf\"/" multi_plot_margin_numu.gnu
+#sed -i'' -e "12s/.*/set output \"${JOBID}\/files_plots\/MINOS_plots_numu.pdf\"/" multi_plot_margin_numu.gnu
 
-sed -i'' -e "117s/.*/set label 35 '+' at $BF_NUMU_S2T,$BF_NUMU_DM2*1e3 center font 'CharterBT-Roman,15'/" multi_plot_margin_numu.gnu
+#sed -i'' -e "117s/.*/set label 35 '+' at $BF_NUMU_S2T,$BF_NUMU_DM2*1e3 center font 'CharterBT-Roman,15'/" multi_plot_margin_numu.gnu
 
-sed -i'' -e "119s/.*/min = $BF_NUMU_CHI2/" multi_plot_margin_numu.gnu
+#sed -i'' -e "119s/.*/min = $BF_NUMU_CHI2/" multi_plot_margin_numu.gnu
 
-sed -i'' -e "48s/.*/plot '${JOBID}\/data\/numu_dm2_chi2.txt' u 2:(10**3*(\$1)) w l lw 2/" multi_plot_margin_numu.gnu
+#sed -i'' -e "48s/.*/plot '${JOBID}\/data\/numu_dm2_chi2.txt' u 2:(10**3*(\$1)) w l lw 2/" multi_plot_margin_numu.gnu
 
-sed -i'' -e "76s/.*/plot '${JOBID}\/data\/numu_s2t_chi2.txt' u 1:2 w l lw 2 t \"\", 16.0 lt 2 lw 2 t \"99.99% C.L. (4{\/Symbol s})\", 9.0 lt 3 lw 2 t \"99.73% C.L. (3{\/Symbol s})\", 4.0 lt 4 lw 2 t \"95.45% C.L. (2{\/Symbol s})\", 2.71 lt 5 lw 2 t \"90.00% C.L.\", 1.0 lt 6 lw 2 t \"68.27% C.L. (1{\/Symbol s})\"/" multi_plot_margin_numu.gnu
+#sed -i'' -e "76s/.*/plot '${JOBID}\/data\/numu_s2t_chi2.txt' u 1:2 w l lw 2 t \"\", 16.0 lt 2 lw 2 t \"99.99% C.L. (4{\/Symbol s})\", 9.0 lt 3 lw 2 t \"99.73% C.L. (3{\/Symbol s})\", 4.0 lt 4 lw 2 t \"95.45% C.L. (2{\/Symbol s})\", 2.71 lt 5 lw 2 t \"90.00% C.L.\", 1.0 lt 6 lw 2 t \"68.27% C.L. (1{\/Symbol s})\"/" multi_plot_margin_numu.gnu
 
-sed -i'' -e "127s/.*/splot '${JOBID}\/data\/numu_chi2_s2t-dm2_surface-noFL.txt' u 1:(10**3*(\$2)):((\$3)-min) w l lw 2/" multi_plot_margin_numu.gnu
+#sed -i'' -e "127s/.*/splot '${JOBID}\/data\/numu_chi2_s2t-dm2_surface-noFL.txt' u 1:(10**3*(\$2)):((\$3)-min) w l lw 2/" multi_plot_margin_numu.gnu
 
-echo
-echo '--------------- numubar ---------------'
+#echo
+#echo '--------------- numubar ---------------'
 
-sed -i'' -e "12s/.*/set output \"${JOBID}\/files_plots\/MINOS_plots_numub.pdf\"/" multi_plot_margin_numub.gnu
+#sed -i'' -e "12s/.*/set output \"${JOBID}\/files_plots\/MINOS_plots_numub.pdf\"/" multi_plot_margin_numub.gnu
 
-sed -i'' -e "117s/.*/set label 35 '+' at $BF_NUMUB_S2T,$BF_NUMUB_DM2*1e3 center font 'CharterBT-Roman,15'/" multi_plot_margin_numub.gnu
+#sed -i'' -e "117s/.*/set label 35 '+' at $BF_NUMUB_S2T,$BF_NUMUB_DM2*1e3 center font 'CharterBT-Roman,15'/" multi_plot_margin_numub.gnu
 
-sed -i'' -e "119s/.*/min = $BF_NUMUB_CHI2/" multi_plot_margin_numub.gnu
+#sed -i'' -e "119s/.*/min = $BF_NUMUB_CHI2/" multi_plot_margin_numub.gnu
 
-sed -i'' -e "48s/.*/plot '${JOBID}\/data\/numub_dm2_chi2.txt' u 2:(10**3*(\$1)) w l lw 2/" multi_plot_margin_numub.gnu
+#sed -i'' -e "48s/.*/plot '${JOBID}\/data\/numub_dm2_chi2.txt' u 2:(10**3*(\$1)) w l lw 2/" multi_plot_margin_numub.gnu
 
-sed -i'' -e "76s/.*/plot '${JOBID}\/data\/numub_s2t_chi2.txt' u 1:2 w l lw 2 t \"\", 16.0 lt 6 lw 2 t \"99.99% C.L. (4{\/Symbol s})\", 9.0 lt 2 lw 2 t \"99.73% C.L. (3{\/Symbol s})\", 4.0 lt 3 lw 2 t \"95.45% C.L. (2{\/Symbol s})\", 2.71 lt 5 lw 2 t \"90.00% C.L.\", 1.0 lt 4 lw 2 t \"68.27% C.L. (1{\/Symbol s})\"/" multi_plot_margin_numub.gnu
+#sed -i'' -e "76s/.*/plot '${JOBID}\/data\/numub_s2t_chi2.txt' u 1:2 w l lw 2 t \"\", 16.0 lt 6 lw 2 t \"99.99% C.L. (4{\/Symbol s})\", 9.0 lt 2 lw 2 t \"99.73% C.L. (3{\/Symbol s})\", 4.0 lt 3 lw 2 t \"95.45% C.L. (2{\/Symbol s})\", 2.71 lt 5 lw 2 t \"90.00% C.L.\", 1.0 lt 4 lw 2 t \"68.27% C.L. (1{\/Symbol s})\"/" multi_plot_margin_numub.gnu
 
-sed -i'' -e "127s/.*/splot '${JOBID}\/data\/numub_chi2_s2t-dm2_surface-noFL.txt' u 1:(10**3*(\$2)):((\$3)-min) w l lw 2/" multi_plot_margin_numub.gnu
+#sed -i'' -e "127s/.*/splot '${JOBID}\/data\/numub_chi2_s2t-dm2_surface-noFL.txt' u 1:(10**3*(\$2)):((\$3)-min) w l lw 2/" multi_plot_margin_numub.gnu
 
-echo '--------------- numubarWS ---------------'
+#echo '--------------- numubarWS ---------------'
 
-sed -i'' -e "12s/.*/set output \"${JOBID}\/files_plots\/MINOS_plots_numubWS.pdf\"/" multi_plot_margin_numubWS.gnu
+#sed -i'' -e "12s/.*/set output \"${JOBID}\/files_plots\/MINOS_plots_numubWS.pdf\"/" multi_plot_margin_numubWS.gnu
 
-sed -i'' -e "117s/.*/set label 35 '+' at $BF_NUMUBWS_S2T,$BF_NUMUBWS_DM2*1e3 center font 'CharterBT-Roman,15'/" multi_plot_margin_numubWS.gnu
+#sed -i'' -e "117s/.*/set label 35 '+' at $BF_NUMUBWS_S2T,$BF_NUMUBWS_DM2*1e3 center font 'CharterBT-Roman,15'/" multi_plot_margin_numubWS.gnu
 
-sed -i'' -e "119s/.*/min = $BF_NUMUBWS_CHI2/" multi_plot_margin_numubWS.gnu
+#sed -i'' -e "119s/.*/min = $BF_NUMUBWS_CHI2/" multi_plot_margin_numubWS.gnu
 
-sed -i'' -e "48s/.*/plot '${JOBID}\/data\/numubWS_dm2_chi2.txt' u 2:(10**3*(\$1)) w l lw 2/" multi_plot_margin_numubWS.gnu
+#sed -i'' -e "48s/.*/plot '${JOBID}\/data\/numubWS_dm2_chi2.txt' u 2:(10**3*(\$1)) w l lw 2/" multi_plot_margin_numubWS.gnu
 
-sed -i'' -e "76s/.*/plot '${JOBID}\/data\/numubWS_s2t_chi2.txt' u 1:2 w l lw 2 t \"\", 16.0 lt 6 lw 2 t \"99.99% C.L. (4{\/Symbol s})\", 9.0 lt 2 lw 2 t \"99.73% C.L. (3{\/Symbol s})\", 4.0 lt 3 lw 2 t \"95.45% C.L. (2{\/Symbol s})\", 2.71 lt 5 lw 2 t \"90.00% C.L.\", 1.0 lt 4 lw 2 t \"68.27% C.L. (1{\/Symbol s})\"/" multi_plot_margin_numubWS.gnu
+#sed -i'' -e "76s/.*/plot '${JOBID}\/data\/numubWS_s2t_chi2.txt' u 1:2 w l lw 2 t \"\", 16.0 lt 6 lw 2 t \"99.99% C.L. (4{\/Symbol s})\", 9.0 lt 2 lw 2 t \"99.73% C.L. (3{\/Symbol s})\", 4.0 lt 3 lw 2 t \"95.45% C.L. (2{\/Symbol s})\", 2.71 lt 5 lw 2 t \"90.00% C.L.\", 1.0 lt 4 lw 2 t \"68.27% C.L. (1{\/Symbol s})\"/" multi_plot_margin_numubWS.gnu
 
-sed -i'' -e "127s/.*/splot '${JOBID}\/data\/numubWS_chi2_s2t-dm2_surface-noFL.txt' u 1:(10**3*(\$2)):((\$3)-min) w l lw 2/" multi_plot_margin_numubWS.gnu
+#sed -i'' -e "127s/.*/splot '${JOBID}\/data\/numubWS_chi2_s2t-dm2_surface-noFL.txt' u 1:(10**3*(\$2)):((\$3)-min) w l lw 2/" multi_plot_margin_numubWS.gnu
 
-echo
+#echo
 
 #---------------------------------------------------------------
 #Execute gnuplot script
@@ -177,9 +177,9 @@ echo '=========================================='
 echo 'Runnign gnuplot macros '
 echo '=========================================='
 echo
-gnuplot multi_plot_margin_numu.gnu
-gnuplot multi_plot_margin_numub.gnu
-gnuplot multi_plot_margin_numubWS.gnu
+#gnuplot multi_plot_margin_numu.gnu
+#gnuplot multi_plot_margin_numub.gnu
+#gnuplot multi_plot_margin_numubWS.gnu
 
 #gnuplot multi_plot_margin_compare.gnu
 #gnuplot multi_plot_margin_compare.gnu
---
+++ MINOS/load-modules.sh
@@ -0,0 +1,4 @@
+#Setup ROOT
+module load lamod/gcc/12.2
+module load lamod/ROOT/v6-26-10
+
